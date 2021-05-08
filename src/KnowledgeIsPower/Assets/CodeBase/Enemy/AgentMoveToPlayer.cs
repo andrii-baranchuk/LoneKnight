13f79dcbab4a96a2c6ec162890c1d11116a24f0d
Andrii Baranchuk <andrii.baranchuk@meliorgames.com>
2021-05-08 15:18:52 +0300

    Enemies. Added EnemyAnimator, MoveToPlayer, AnimationAlongAgent,AggroZone, TriggerObserver.

---
+++ src/KnowledgeIsPower/Assets/CodeBase/Enemy/AgentMoveToPlayer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using CodeBase.Infrastructure.Factory;
+using CodeBase.Infrastructure.Services;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace CodeBase.Enemy
+{
+  public class AgentMoveToPlayer : Follow
+  {
+    private const float MinimalDistance = 1f;
+
+    public NavMeshAgent Agent;
+
+    private IGameFactory _gameFactory;
+    private Transform _heroTransform;
+
+    private void Start()
+    {
+      _gameFactory = AllServices.Container.Single<IGameFactory>();
+
+      if (_gameFactory.HeroGameObject != null)
+        InitializeHeroTransform();
+      else
+      {
+        _gameFactory.HeroCreated += HeroCreated;
+      }
+    }
+
+    private void Update()
+    {
+      if (HeroInitialized() && HeroNotReached())
+        Agent.destination = _heroTransform.position;
+    }
+
+    private bool HeroInitialized() => 
+      _heroTransform != null;
+
+    private void InitializeHeroTransform() =>
+      _heroTransform = _gameFactory.HeroGameObject.transform;
+
+    private void HeroCreated() =>
+      InitializeHeroTransform();
+
+    private bool HeroNotReached() =>
+      Vector3.Distance(Agent.transform.position, _heroTransform.position) >= MinimalDistance;
+  }
+}
---
+++ src/KnowledgeIsPower/Assets/CodeBase/Enemy/RotateToPlayer.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using CodeBase.Infrastructure.Factory;
+using CodeBase.Infrastructure.Services;
+using UnityEngine;
+
+namespace CodeBase.Enemy
+{
+  public class RotateToPlayer : Follow
+  {
+    private float Speed = 1f;
+
+    private IGameFactory _gameFactory;
+    private Transform _heroTransform;
+    private Vector3 _positionToLook;
+
+    private void Start()
+    {
+      _gameFactory = AllServices.Container.Single<IGameFactory>();
+
+      if (_gameFactory.HeroGameObject != null)
+        InitializeHeroTransform();
+      else
+      {
+        _gameFactory.HeroCreated += HeroCreated;
+      }
+    }
+
+    private void Update()
+    {
+      if(HeroInitialized())
+        RotateTowardsHero();
+    }
+
+    private void InitializeHeroTransform() =>
+      _heroTransform = _gameFactory.HeroGameObject.transform;
+    
+    private void HeroCreated() =>
+      InitializeHeroTransform();
+    
+    private bool HeroInitialized() => 
+      _heroTransform != null;
+
+    private void RotateTowardsHero()
+    {
+      UpdatePositionToLookAt();
+
+      transform.rotation = SmoothRotation(transform.rotation, _positionToLook);
+    }
+
+    private void UpdatePositionToLookAt()
+    {
+      Vector3 positionDiff = _heroTransform.position - transform.position;
+      _positionToLook = new Vector3(positionDiff.x, transform.position.y, positionDiff.z);
+    }
+
+    private Quaternion SmoothRotation(Quaternion rotation, Vector3 positionToLook)
+    {
+      return Quaternion.Lerp(rotation, TargetRotation(positionToLook), SpeedFactor());
+    }
+
+    private Quaternion TargetRotation(Vector3 position) => 
+      Quaternion.LookRotation(position);
+
+    private float SpeedFactor() => 
+      Speed * Time.deltaTime;
+  }
+}
